537d2d701c075d596db6bd9115d17f35892adce4
Tijo <144551901+tijo95@users.noreply.github.com>
2023-11-16 00:17:47 +0100

    Update script.py

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import json 
 from modules import shared
 import os
+import re
 
 settings_file = 'extensions/win_tts_piper/settings.json'
 
@@ -14,9 +15,11 @@ params = {
     "autoplay": True,
     "show_text": True,
     "selected_model": "",
+    "speaker_id": 0,
     "noise_scale": 0.667,
     "length_scale": 1.0,
     "noise_w": 0.8,
+    "sentence_silence": 0.2,
     "ignore_asterisk_text": False,    
 }
 
@@ -39,7 +42,8 @@ def clean_text(text):
     cleaned_text = cleaned_text.replace('&#x27;', "'").replace('&quot;', '"')
     cleaned_text = cleaned_text.replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>')
     cleaned_text = cleaned_text.replace('&nbsp;', ' ').replace('&copy;', '©').replace('&reg;', '®')
-
+    cleaned_text = re.sub(r"[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002702-\U000027B0\U000024C2-\U0001F251]+", "", cleaned_text)
+    
     # Ignorer le texte entre astérisques si l'option est activée
     if params["ignore_asterisk_text"]:
         while '*' in cleaned_text:
@@ -48,9 +52,9 @@ def clean_text(text):
             if start != -1 and end != -1 and start < end:
                 excluded_text = cleaned_text[start:end + 1]
                 cleaned_text = cleaned_text.replace(excluded_text, '')
-
-    # Ajoutez d'autres remplacements au besoin
+  
     return cleaned_text
+
 
 def tts(text, output_file):
     cleaned_text = clean_text(text)
@@ -65,9 +69,11 @@ def tts(text, output_file):
     process = subprocess.Popen(
         [
             piper_path.as_posix(),
+            '--sentence_silence', str(params['sentence_silence']),
             '--noise_scale', str(params['noise_scale']),
             '--length_scale', str(params['length_scale']),
             '--noise_w', str(params['noise_w']),
+            '--speaker', str(params['speaker_id']),
             '--model', model_path.as_posix(),
             '--output_file', output_file_str,
         ],
@@ -132,17 +138,16 @@ def set_initial_model():
     model_folder = Path('extensions/win_tts_piper/model')
     available_models = [model.name for model in model_folder.glob('*.onnx')]
 
-    # Charge les paramètres depuis le fichier JSON
     load_settings()
 
-    if available_models and not params["selected_model"]:
-        # Utilise le modèle du fichier JSON si disponible
+    if not params["selected_model"] and available_models:
         initial_model = params.get("selected_model", available_models[0])
+        
         params.update({
             "selected_model": initial_model,
-            "active": True,
-            "autoplay": True,
-            "show_text": True
+            "active": params.get("active", True),
+            "autoplay": params.get("autoplay", True),
+            "show_text": params.get("show_text", True),
         })
 
 # Appelle set_initial_model()
@@ -154,9 +159,11 @@ def save_settings():
         "autoplay": params["autoplay"],
         "show_text": params["show_text"],
         "selected_model": params["selected_model"],
+        "speaker_id": params["speaker_id"],
         "noise_scale": params["noise_scale"],
         "length_scale": params["length_scale"],
         "noise_w": params["noise_w"],
+        "sentence_silence": params["sentence_silence"],
         "ignore_asterisk_text": params["ignore_asterisk_text"],
     }
 
@@ -177,6 +184,7 @@ def ui():
         noise_scale_slider = gr.Slider(minimum=0.0, maximum=1.0, label=f'Noise Scale : Default (0.66)', value=params['noise_scale'])
         length_scale_slider = gr.Slider(minimum=0.0, maximum=2.0, label='Length Scale : Default (1)', value=params['length_scale'])
         noise_w_slider = gr.Slider(minimum=0.0, maximum=1.0, label='Noise Width : Default (0.8)', value=params['noise_w'])
+        sentence_silence_slider = gr.Slider(minimum=0.0, maximum=1.0, label='Sentence Silence : Default (0.2)', value=params['sentence_silence'])
         
         activate.change(lambda x: params.update({'active': x}), activate, None)
         autoplay.change(lambda x: params.update({'autoplay': x}), autoplay, None)
@@ -186,9 +194,14 @@ def ui():
         noise_scale_slider.change(lambda x: params.update({'noise_scale': x}), noise_scale_slider, None)
         length_scale_slider.change(lambda x: params.update({'length_scale': x}), length_scale_slider, None)
         noise_w_slider.change(lambda x: params.update({'noise_w': x}), noise_w_slider, None)
+        sentence_silence_slider.change(lambda x: params.update({'sentence_silence': x}), sentence_silence_slider, None)
         
         # Utilise params["selected_model"] comme valeur initiale du menu déroulant
         model_dropdown = create_model_dropdown(model_folder)
+        
+        speaker_id_input = gr.Number(value=params["speaker_id"], label="Speaker ID : Default (0) See the model JSON file to find out which ID are available for the selected model.")
+        speaker_id_input.change(lambda x: params.update({'speaker_id': int(x)}), speaker_id_input, None)
+
         
         save_button = gr.Button("Save Settings")
         save_button.click(save_settings, None)
