a8e7e4f5e434d15569c87516b7d63f62461593d1
Tijo <144551901+tijo95@users.noreply.github.com>
2023-11-18 22:53:27 +0100

    Update script.py

--- script.py
+++ script.py
@@ -40,9 +40,22 @@ output_folder = Path('extensions/win_tts_piper/outputs')
 
 def clean_text(text):
     cleaned_text = text
-    cleaned_text = cleaned_text.replace('&#x27;', "'").replace('&quot;', '"')
-    cleaned_text = cleaned_text.replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>')
-    cleaned_text = cleaned_text.replace('&nbsp;', ' ').replace('&copy;', '©').replace('&reg;', '®')
+    
+    replacements = {
+        '&#x27;': "'",
+        '&quot;': '"',
+        '&amp;': '&',
+        '&lt;': '<',
+        '&gt;': '>',
+        '&nbsp;': ' ',
+        '&copy;': '©',
+        '&reg;': '®'
+    }
+
+    for key, value in replacements.items():
+        cleaned_text = cleaned_text.replace(key, value)
+        
+    cleaned_text = cleaned_text.replace("***", "*").replace("**", "*")
     cleaned_text = re.sub(r"[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002702-\U000027B0\U000024C2-\U0001F251]+", "", cleaned_text)
     
     # Ignorer le texte entre astérisques si l'option est activée
